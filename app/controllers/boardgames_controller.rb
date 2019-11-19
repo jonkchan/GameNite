28a1947d4f810f54125210f3163386d9356f3969
Jonathan Chan <johnkchan@users.noreply.github.com>
2019-11-19 13:07:32 -0500

    Added Categories

--- app/controllers/boardgames_controller.rb
+++ app/controllers/boardgames_controller.rb
@@ -5,16 +5,21 @@ class BoardgamesController < ApplicationController
     @data = boardgame_object.get_details
   end
 
+  # Route to show all categories
   def categories
-    @category = params[:category]
-    
-    if @category
-      response = HTTParty.get('https://www.boardgameatlas.com/api/search?categories=' + @category + '&client_id=' + ENV['CLIENT_ID'])
-      @categories = ["name": 1]
-    else
       response = HTTParty.get('https://www.boardgameatlas.com/api/game/categories?&client_id=' + ENV['CLIENT_ID'])
       @categories = response["categories"]
-    end
+  end
+
+  # Route to show games by category
+  def category
+    @category_id = params[:category]
+    response = HTTParty.get('https://www.boardgameatlas.com/api/search?categories=' + @category_id + '&order_by=name&ascending=true&client_id=' + ENV['CLIENT_ID'])
+    @boardgames = response["games"]
+  end
+
+  def about
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # get 'boardgame/index'
   # resources :boardgame
   get 'categories', to: 'boardgames#categories'
-  get 'categories/:category', to: 'boardgames#categories'
+  get 'categories/:category', to: 'boardgames#category'
+
+  get 'about', to: 'boardgames#about'
+
   root 'boardgames#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
