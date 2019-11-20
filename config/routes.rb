58acd095e9bd0f1bc51ff934e2e9174f547a3ec8
Jonathan Chan <johnkchan@users.noreply.github.com>
2019-11-19 23:08:08 -0500

    Updated user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # get 'boardgame/index'
   # resources :boardgame
   get 'categories', to: 'boardgames#categories'
@@ -7,9 +8,6 @@ Rails.application.routes.draw do
   get 'boardgames/:boardgame_id', to: 'boardgames#show'
 
   get 'about', to: 'boardgames#about'
-
-  get 'users', to: 'user#index'
-  get 'users/:username', to: 'users#show'
 
   root 'boardgames#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
